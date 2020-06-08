13821d11f33c464402d6ab640d58b89a8b4eda57
Harriet Goddard <goddardhe@gmail.com>
2020-06-08 14:08:14 +0100

    Initial state successfully renders

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from "redux";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+
+const initial = {
+  player1: 0,
+  player2: 0,
+};
+
+const reducer = state => {
+  return state;
+}
+
+const store = createStore(
+  reducer,
+  initial,
+  window.__REDUX_DEVTOOLS_EXTENSION__
+    && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const render = () => {
+  let state = store.getState();
+
+  ReactDOM.render(
+    <React.StrictMode>
+      <App player1={ state.player1 } player2={ state.player2 }/>
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+
+}
+
+store.subscribe(render);
+render();
+
+
+
